d8baea8e4a5a56393de53c3b5c5f376eb17e3515
ApOxX <nourdinearbaoui@gmail.com>
2024-02-09 17:08:44 +0100

    refacto test createCard

--- src/main/kotlin/org/clean/leitner/domain/CreateCard.kt
+++ src/main/kotlin/org/clean/leitner/domain/CreateCard.kt
@@ -1,8 +1,22 @@
 package org.clean.leitner.domain
 
+import org.clean.leitner.domain.model.Card
+import org.clean.leitner.domain.model.CardId
+import org.clean.leitner.domain.model.CardUserData
+import org.clean.leitner.domain.model.Category
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class CreateCard {
 
+    fun create(cardUserData: CardUserData): Card {
+        return Card(
+            CardId(UUID.randomUUID().toString()),
+            Category.FIRST,
+            cardUserData.question,
+            cardUserData.answer,
+            cardUserData.tag
+        )
+    }
 }
